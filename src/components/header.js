0095edc198e1ccca0b5003996e646047be9f1ee5
huahuadavids <1457896625@qq.com>
2018-08-08 11:24:13 +0800

    init

---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+class Header extends  React.Component {
+  constructor(){
+    super();
+    this.state = {
+      text: "hello"
+    }
+  }
+  render(){
+    const lists = [ "home", "intro", "login" ];
+    const headers = lists.map( (value,index,array)=> {
+      return <li key={value+Math.random()}>{value}</li>
+    });
+
+    return (
+      <header id="header">
+          <ul>
+            {headers}
+          </ul>
+          <div>
+            {this.state.text}
+          </div>
+      </header>
+    )
+  }
+}
+export default Header;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import logo                 from '../logo.svg';
+import '../styles/App.css';
+import '../styles/common.styl'
+import Header               from "../components/header"
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header></Header>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
